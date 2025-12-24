f1a31c262a7d1d01b1a0f9b17e1bd7360a337c32
dev-seawiid <seatide2215@gmail.com>
2025-12-24 16:52:16 +0900

    feat: projectsData에 새로운 프로젝트 추가
    - 여러 웹 프로젝트에 대한 정보를 projectsData.ts에 추가
    - 각 프로젝트의 제목, 설명, 링크 포함
    - 기존 주석 처리된 항목 제거

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,15 +6,55 @@ interface Project {
 }
 
 const projectsData: Project[] = [
+  {
+    title: '[운영] 해조류_SEAWIID',
+    description: `싱어송라이터 해조류 소개 웹페이지`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://www.seawiid.com/',
+  },
+  {
+    title: '[운영] 시흥버스킹',
+    description: `'시흥시 아마추어 버스킹 모임' 일정 공유 웹페이지`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://www.shbusking.com/',
+  },
+  {
+    title: '[업무] 프리비아 항공 > 예약',
+    description: `프리비아 항공 NextJS 마이그레이션 > 예약 페이지 개발`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://www.priviatravel.com/flight',
+  },
+  {
+    title: '[업무] 카이트 > 투어&티켓',
+    description: `투어&티켓 서비스 > 예약 및 예약상세 페이지 개발`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://kyte.travel/tours',
+  },
+  {
+    title: '[업무] 플레이윙즈 > 유지보수',
+    description: `플레이윙즈 크로스브라우징 이슈 수정 및 유지보수`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://www.playwings.co.kr/',
+  },
+  {
+    title: '[웹 도구] 카풀 매칭',
+    description: `거리 및 시간 기반 카풀 매칭 웹 도구`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://www.seawiid.com/church/carpool',
+  },
+  {
+    title: '[웹 도구] 성구암송',
+    description: `스와이프 / 작성내용 비교 > 성구 암송을 도와주는 웹 도구`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://www.seawiid.com/disciples/recitations/love4or',
+  },
+  {
+    title: '[웹 도구] 그룹 나누기',
+    description: `소켓 기반으로 접속자를 n개의 그룹으로 이븐하게 나눠주는 웹 도구`,
+    // imgSrc: '/static/images/twitter-card.png',
+    href: 'https://www.seawiid.com/tools/grouping',
+  },
   // TODO: 추가 예정
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
   // {
   //   title: 'The Time Machine',
   //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
