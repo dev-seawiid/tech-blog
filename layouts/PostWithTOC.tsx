6014893328bbd5750bdb83391bb708fb9feaf1b3
dev-seawiid <seatide2215@gmail.com>
2026-01-08 16:47:12 +0900

    refactor: PostWithTOC 레이아웃 개선 및 스타일 수정
    - PostWithTOC 컴포넌트에서 최소 너비 설정 추가
    - 콘텐츠 영역에 overflow-x-auto 클래스를 추가하여 가로 스크롤 가능하도록 수정

--- layouts/PostWithTOC.tsx
+++ layouts/PostWithTOC.tsx
@@ -55,8 +55,10 @@ export default function PostWithTOC({ content, next, prev, children, toc = [] }:
               </div>
             </aside>
           )}
-          <div className="divide-y divide-gray-200 xl:flex-1 xl:pb-0 dark:divide-gray-700">
-            <div className="prose dark:prose-invert max-w-none pt-10 pb-8">{children}</div>
+          <div className="divide-y divide-gray-200 xl:min-w-0 xl:flex-1 xl:pb-0 dark:divide-gray-700">
+            <div className="prose dark:prose-invert max-w-none overflow-x-auto pt-10 pb-8">
+              {children}
+            </div>
             {siteMetadata.comments && (
               <div className="pt-6 pb-6 text-center text-gray-700 dark:text-gray-300" id="comment">
                 <Comments slug={slug} />
