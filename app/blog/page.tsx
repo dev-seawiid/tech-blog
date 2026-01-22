782cdbcaa5217fcaf6e23c68212927434938bfec
dev-seawiid <seatide2215@gmail.com>
2026-01-22 11:44:27 +0900

    fix: 정적 페이지 빌드 오류 수정
    - BlogPage 컴포넌트에서 props로 전달된 searchParams를 제거하고, 기본 페이지 번호를 1로 설정하여 초기 표시 게시물을 간소화했습니다.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,15 +7,11 @@ const POSTS_PER_PAGE = 5
 
 export const metadata = genPageMetadata({ title: 'Blog' })
 
-export default async function BlogPage(props: { searchParams: Promise<{ page: string }> }) {
+export default function BlogPage() {
   const posts = allCoreContent(sortPosts(allBlogs))
-  const searchParams = await props.searchParams
-  const pageNumber = searchParams.page ? parseInt(searchParams.page) : 1
   const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = posts.slice(
-    POSTS_PER_PAGE * (pageNumber - 1),
-    POSTS_PER_PAGE * pageNumber
-  )
+  const pageNumber = 1
+  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE * pageNumber)
   const pagination = {
     currentPage: pageNumber,
     totalPages: totalPages,
