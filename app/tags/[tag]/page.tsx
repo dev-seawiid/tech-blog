12b16f747fa85948b805f89fdae574d76a552b57
dev-seawiid <seatide2215@gmail.com>
2026-01-24 16:01:17 +0900

    fix: git page 배포 시 /tag/회고 등 한글 태그 접근 시 404 페이지가 뜨는 이슈 수정
    - "serve:pages" 에서 로컬 테스트 가능

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -5,7 +5,6 @@ import tagData from 'app/tag-data.json'
 import { allBlogs } from 'contentlayer/generated'
 import { slug } from 'github-slugger'
 import { Metadata } from 'next'
-import { notFound } from 'next/navigation'
 import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 
 const POSTS_PER_PAGE = 5
@@ -31,7 +30,7 @@ export const generateStaticParams = async () => {
   const tagCounts = tagData as Record<string, number>
   const tagKeys = Object.keys(tagCounts)
   return tagKeys.map((tag) => ({
-    tag: encodeURI(tag),
+    tag: tag,
   }))
 }
 
